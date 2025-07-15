fe56bbb4897317b0bd04faaf21521bb03d99ffdd
Erick-Henrique-Pedrini <erickpedrinebr@gmail.com>
2025-07-15 14:45:46 -0300

    refactor: Orgazição dos types #9

--- server/src/types/userTypes.ts
+++ server/src/types/userTypes.ts
@@ -1,3 +1,4 @@
+
 export interface TypeReportType {
   id_type_report: number;
   name_type_report: string;
@@ -6,53 +7,6 @@ export interface TypeReportType {
 export interface TypeReviewType {
   id_type_review: number;
   name_type_review: string;
-}
-
-
-export interface UserType {}
-export interface ReportType {}
-export interface ReviewType {}
-export interface ReportCommentType {}
-export interface ReviewCommentType {}
-
-
-export interface ReportCommentType {
-  id_report_comment: number;
-  content_report_comment: string;
-  user: UserType;
-  report: ReportType;
-}
-
-
-export interface ReviewCommentType {
-  id_review_comment: number;
-  content_review_comment: string;
-  user: UserType;
-  review: ReviewType;
-}
-
-
-export interface ReportType {
-  id_report: number;
-  user: UserType;
-  type: TypeReportType;
-  id_state: number;
-  id_city: number;
-  id_neighborhood: number;
-  content_report: string;
-  comments?: ReportCommentType[];
-}
-
-
-export interface ReviewType {
-  id_review: number;
-  user: UserType;
-  type: TypeReviewType;
-  id_state: number;
-  id_city: number;
-  id_neighborhood: number;
-  content_review: string;
-  comments?: ReviewCommentType[];
 }
 
 
@@ -68,4 +22,40 @@ export interface UserType {
   reviews?: ReviewType[];
   reportComments?: ReportCommentType[];
   reviewComments?: ReviewCommentType[];
+}
+
+export interface ReportCommentType {
+  id_report_comment: number;
+  content_report_comment: string;
+  user: UserType;
+  report: ReportType;
+}
+
+export interface ReviewCommentType {
+  id_review_comment: number;
+  content_review_comment: string;
+  user: UserType;
+  review: ReviewType;
+}
+
+export interface ReportType {
+  id_report: number;
+  user: UserType;
+  type: TypeReportType;
+  id_state: number;
+  id_city: number;
+  id_neighborhood: number;
+  content_report: string;
+  comments?: ReportCommentType[];
+}
+
+export interface ReviewType {
+  id_review: number;
+  user: UserType;
+  type: TypeReviewType;
+  id_state: number;
+  id_city: number;
+  id_neighborhood: number;
+  content_review: string;
+  comments?: ReviewCommentType[];
 }
