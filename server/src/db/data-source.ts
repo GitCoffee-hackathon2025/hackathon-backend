fbc667c09a35fdc4c91edac894cc83eda31301ab
Chormiak <william.chormi@gmail.com>
2025-07-23 13:59:16 -0300

    CHORE: adicionando comentário para usar o dotenv no deploy
    Signed-off-by: Chormiak <william.chormi@gmail.com>

--- server/src/db/data-source.ts
+++ server/src/db/data-source.ts
@@ -1,6 +1,11 @@
 import { DataSource } from "typeorm";
 import { UserEntity, ReportEntity, ReviewEntity, TypeReportEntity, TypeReviewEntity, ReportCommentEntity, ReviewCommentEntity } from "../entities/userEntities"; // Ajuste o caminho conforme necessário
 
+/* ============================
+
+Na hora de fazer o deploy é nessário importar o dotenv neste arquivo, e PELO MENOS usar o process.env em `userName` e `password`
+
+============================ */
 export const AppDataSource = new DataSource({
     type: "mysql", 
     host: "localhost",
