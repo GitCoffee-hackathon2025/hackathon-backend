542c6d0423bf10b4e6f75070040d80024198246c
IanMV <ianmatheusv54@gmail.com>
2025-08-31 13:34:37 -0300

    FEAT: Variáveis de ambiente no 'data-source.ts'

--- server/src/db/data-source.ts
+++ server/src/db/data-source.ts
@@ -1,5 +1,6 @@
 import { DataSource } from "typeorm";
 import { UserEntity, ReportEntity, ReviewEntity, TypeReportEntity, TypeReviewEntity, ReportCommentEntity, ReviewCommentEntity } from "../entities/userEntities"; // Ajuste o caminho conforme necessário
+import 'dotenv/config';
 
 /* ============================
 
@@ -8,10 +9,10 @@ Na hora de fazer o deploy é nessário importar o dotenv neste arquivo, e PELO M
 ============================ */
 export const AppDataSource = new DataSource({
     type: "mysql", 
-    host: "localhost",
+    host: process.env.HOST,
     port: 3306,
-    username: "root",
-    password: "root",
+    username: process.env.USER,
+    password: process.env.PASSWORD,
     database: "hackathon",
     synchronize: true, 
     logging: true,
