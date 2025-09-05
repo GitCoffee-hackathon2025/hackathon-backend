317636802f4161080df51a83cbfc06e397e2207a
IanMV <ianmatheusv54@gmail.com>
2025-09-04 23:11:24 -0300

    REFACT:  Funções relacionadas ao hash para melhor reutilização

---
+++ server/src/utils/crypto.ts
@@ -0,0 +1,12 @@
+import bcrypt from "bcrypt";
+
+export class CryptoUtil {
+  static async hashPassword(password: string): Promise<string> {
+    const saltRounds = 12;
+    return bcrypt.hash(password, saltRounds);
+  }
+
+  static async comparePassword(password: string, hash: string): Promise<boolean> {
+    return bcrypt.compare(password, hash);
+  }
+}
